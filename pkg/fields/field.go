340570e111e9fdd98f437e6d29f22fce494b6656
Joel Ling <joel-ling@protonmail.com>
2021-11-30 20:09:19 +0900

    define package field with i/face and impl.

---
+++ pkg/fields/field.go
@@ -0,0 +1,156 @@
+package fields
+
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/joel-ling/go-bitfields/pkg/structs/errors"
+)
+
+type Field interface {
+	// A Field represents a sequence of bits in a binary message or file format.
+
+	LengthInBits() uint
+	// Return the number of bits in a Field.
+
+	OffsetInBits() uint
+	// Return the number of bits to the right of the rightmost bit of a Field
+	// in a Word.
+}
+
+type field struct {
+	lengthInBits uint
+	offsetInBits uint
+}
+
+func NewFieldFromStructField(structField reflect.StructField,
+	wordName, formatName string,
+	sumFieldLengthsBefore uint,
+) (
+	f *field, sumFieldLengthsAfter uint, e error,
+) {
+	// Return a default implementation of the interface Field
+	// and a cumulative sum of the length of some collection of Fields,
+	// given a reflection of a struct field representing a Field in a Word,
+	// the names of the Format and Word containing that Field,
+	// and the previous cumulative sum of the length of those Fields.
+
+	const (
+		structFieldTagKey         = "bitfield"
+		structFieldTagValueFormat = "%d,%d"
+
+		sizeBool   = 1
+		sizeUint   = 64
+		sizeUint16 = 16
+		sizeUint32 = 32
+		sizeUint64 = 64
+		sizeUint8  = 8
+	)
+
+	var (
+		structFieldTagKeyOK bool
+		structFieldTagValue string
+		structFieldKindSize uint
+	)
+
+	switch structField.Type.Kind() {
+	case reflect.Uint:
+		structFieldKindSize = sizeUint
+
+	case reflect.Uint8:
+		structFieldKindSize = sizeUint8
+
+	case reflect.Uint16:
+		structFieldKindSize = sizeUint16
+
+	case reflect.Uint32:
+		structFieldKindSize = sizeUint32
+
+	case reflect.Uint64:
+		structFieldKindSize = sizeUint64
+
+	case reflect.Bool:
+		structFieldKindSize = sizeBool
+
+	default:
+		e = errors.NewFieldTypeUnsupportedError(
+			formatName,
+			wordName,
+			structField.Name,
+			structField.Type.Kind().String(),
+		)
+
+		return
+	}
+
+	if len(structField.Tag) == 0 {
+		e = errors.NewFieldMissingStructTagError(
+			formatName,
+			wordName,
+			structField.Name,
+		)
+
+		return
+	}
+
+	f = new(field)
+
+	structFieldTagValue, structFieldTagKeyOK = structField.Tag.Lookup(
+		structFieldTagKey,
+	)
+
+	_, e = fmt.Sscanf(
+		structFieldTagValue,
+		structFieldTagValueFormat,
+		&f.lengthInBits,
+		&f.offsetInBits,
+	)
+	if e != nil || !structFieldTagKeyOK {
+		e = errors.NewFieldStructTagMalformedError(
+			formatName,
+			wordName,
+			structField.Name,
+		)
+
+		return
+	}
+
+	if f.lengthInBits > structFieldKindSize {
+		e = errors.NewFieldLengthOverflowsTypeError(
+			formatName,
+			wordName,
+			structField.Name,
+			structField.Type.Kind().String(),
+			structFieldKindSize,
+			f.lengthInBits,
+		)
+
+		return
+	}
+
+	if f.offsetInBits != sumFieldLengthsBefore {
+		e = errors.NewFieldHasGapOrOverlapToRightError(
+			formatName,
+			wordName,
+			structField.Name,
+		)
+
+		return
+	}
+
+	sumFieldLengthsAfter = sumFieldLengthsBefore + f.lengthInBits
+
+	return
+}
+
+func (f *field) LengthInBits() uint {
+	// Implement interface Field.
+
+	return f.lengthInBits
+}
+
+func (f *field) OffsetInBits() uint {
+	// Implement interface Field.
+
+	return f.offsetInBits
+}
---
+++ pkg/fields/field_test.go
@@ -0,0 +1,148 @@
+package fields
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/joel-ling/go-bitfields/pkg/formats/test"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestField(t *testing.T) {
+	const (
+		formatName = "formatName"
+		nCases     = 8
+	)
+
+	var (
+		words = []interface{}{
+			test.TestWord11Bad{},
+			test.TestWord0{},
+			test.TestWord7Bad{},
+			test.TestWord13Bad{},
+			test.TestWord12Bad{},
+			test.TestWord3Bad{},
+			test.TestWord4Bad{},
+			test.TestWord8Bad{},
+		}
+
+		structFieldIndices = []int{
+			0,
+			0,
+			1,
+			4,
+			2,
+			0,
+			1,
+			2,
+		}
+
+		sumsFieldLengthsBefore = []uint{
+			16,
+			16,
+			20,
+			0,
+			11,
+			16,
+			20,
+			27,
+		}
+
+		sumsFieldLengthsAfter = []uint{
+			24,
+			24,
+			27,
+			8,
+			20,
+			24,
+			27,
+			27,
+		}
+
+		lengthsInBits = []uint{
+			8,
+			8,
+			7,
+			8,
+			9,
+			6,
+			9,
+			6,
+		}
+
+		offsetsInBits = []uint{
+			16,
+			16,
+			20,
+			0,
+			11,
+			18,
+			18,
+			21,
+		}
+
+		expectError = []bool{
+			true,
+			false,
+			true,
+			true,
+			true,
+			true,
+			true,
+			true,
+		}
+
+		e               error
+		i               int
+		f               Field
+		typeReflection  reflect.Type
+		sumFieldLengths uint
+	)
+
+	for i = 0; i < nCases; i++ {
+		typeReflection = reflect.TypeOf(words[i])
+
+		f, sumFieldLengths, e = NewFieldFromStructField(
+			typeReflection.Field(structFieldIndices[i]),
+			typeReflection.Name(),
+			formatName,
+			sumsFieldLengthsBefore[i],
+		)
+
+		if expectError[i] {
+			assert.NotNil(t, e,
+				"The constructor of a Field "+
+					"should return an error "+
+					"when it is passed a bad struct field.",
+			)
+
+			t.Log(e)
+
+			continue
+
+		} else {
+			if e != nil {
+				t.Error(e)
+			}
+		}
+
+		assert.Equal(t,
+			lengthsInBits[i], f.LengthInBits(),
+			"The value returned by Field.LengthInBits() "+
+				"should be equal to the relevant value in the struct tag.",
+		)
+
+		assert.Equal(t,
+			offsetsInBits[i], f.OffsetInBits(),
+			"The value returned by Field.OffsetInBits() "+
+				"should be equal to the relevant value in the struct tag.",
+		)
+
+		assert.Equal(t,
+			sumsFieldLengthsAfter[i], sumFieldLengths,
+			"The cumulative sum of Field lengths "+
+				"should be equal to the sum of the previous cumulative sum "+
+				"and the length of the current Field.",
+		)
+	}
+}
