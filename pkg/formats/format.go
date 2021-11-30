1ee4fd9dae4768bc474c320ce13a59c6a7da23a0
Joel Ling <joel-ling@protonmail.com>
2021-11-30 21:16:00 +0900

    define package formats with i/face & impl.

---
+++ pkg/formats/format.go
@@ -0,0 +1,99 @@
+package formats
+
+import (
+	"reflect"
+
+	"github.com/joel-ling/go-bitfields/pkg/structs/errors"
+	"github.com/joel-ling/go-bitfields/pkg/words"
+)
+
+type Format interface {
+	// A Format represents a binary message or file format made up of Words.
+	// See package `words` for the defintion of a Word.
+
+	NWords() uint
+	// Return the number of Words in a Format.
+
+	Word(uint) words.Word
+	// Return a Word given its index in a Format.
+}
+
+type format struct {
+	sliceOfWords []words.Word
+}
+
+func NewFormatFromType(typeReflection reflect.Type) (f *format, e error) {
+	// Return a default implementation of the interface Format,
+	// given a type reflection of a format struct.
+
+	var (
+		i    int
+		word words.Word
+	)
+
+	if typeReflection.Kind() != reflect.Ptr {
+		e = errors.NewInterfaceIsNotPointerError(
+			typeReflection.String(),
+		)
+
+		return
+	}
+
+	typeReflection = typeReflection.Elem()
+
+	if typeReflection.Kind() != reflect.Struct {
+		e = errors.NewFormatIsNotStructError(
+			typeReflection.String(),
+		)
+
+		return
+	}
+
+	if typeReflection.NumField() == 0 {
+		e = errors.NewFormatHasNoWordsError(
+			typeReflection.String(),
+		)
+
+		return
+	}
+
+	f = new(format)
+
+	for i = 0; i < typeReflection.NumField(); i++ {
+		word, e = words.NewWordFromStructField(
+			typeReflection.Field(i),
+			typeReflection.String(),
+		)
+		if e != nil {
+			return
+		}
+
+		f.addWordToSlice(word)
+	}
+
+	return
+}
+
+func (f *format) NWords() (nWords uint) {
+	// Implement interface Format.
+
+	nWords = uint(
+		len(f.sliceOfWords),
+	)
+
+	return
+}
+
+func (f *format) Word(index uint) words.Word {
+	// Implement interface Format.
+
+	return f.sliceOfWords[index]
+}
+
+func (f *format) addWordToSlice(word words.Word) {
+	// Add a Word to f.sliceOfWords.
+
+	f.sliceOfWords = append(f.sliceOfWords, word)
+
+	return
+}
---
+++ pkg/formats/format_test.go
@@ -0,0 +1,97 @@
+package formats
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/joel-ling/go-bitfields/pkg/formats/test"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFormat(t *testing.T) {
+	const (
+		nCases = 13
+	)
+
+	var (
+		testFormat14Bad = test.TestFormat14Bad(0)
+
+		formats = []interface{}{
+			&test.TestFormat0{},
+			&test.TestFormat1Bad{},
+			&test.TestFormat2Bad{},
+			&test.TestFormat5Bad{},
+			&test.TestFormat6Bad{},
+			&test.TestFormat7Bad{},
+			&test.TestFormat8Bad{},
+			&test.TestFormat9Bad{},
+			&test.TestFormat10Bad{},
+			&test.TestFormat11Bad{},
+			&test.TestFormat12Bad{},
+			&test.TestFormat13Bad{},
+			&testFormat14Bad,
+		}
+
+		nWords = []uint{
+			3,
+			3,
+			3,
+			3,
+			3,
+			3,
+			3,
+			0,
+			3,
+			0,
+		}
+
+		expectError = []bool{
+			false,
+			true,
+			true,
+			true,
+			true,
+			true,
+			true,
+			true,
+			true,
+			true,
+			true,
+			true,
+			true,
+		}
+
+		e error
+		f Format
+		i int
+	)
+
+	for i = 0; i < nCases; i++ {
+		f, e = NewFormatFromType(
+			reflect.TypeOf(formats[i]),
+		)
+
+		if expectError[i] {
+			assert.NotNil(t, e,
+				"The constructor of a Format "+
+					"should return an error "+
+					"when it is passed a bad struct.",
+			)
+
+			t.Log(e)
+
+			continue
+
+		} else {
+			if e != nil {
+				t.Error(e)
+			}
+		}
+
+		assert.Equal(t,
+			nWords[i], f.NWords(),
+			"The value returned by Format.NWords() "+
+				"should be equal to the expected value given the struct.",
+		)
+	}
+}
